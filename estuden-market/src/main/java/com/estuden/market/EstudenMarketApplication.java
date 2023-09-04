41da455fa15186d343ff1d22a2d33e5e51990615
TonciP <63807995+TonciP@users.noreply.github.com>
2023-09-03 22:04:19 -0400

    solucion inicial

---
+++ estuden-market/src/main/java/com/estuden/market/EstudenMarketApplication.java
@@ -0,0 +1,15 @@
+package com.estuden.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudenMarketApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		//SpringApplication.run(EstudenMarketApplication.class, args);
+
+	}
+
+}
